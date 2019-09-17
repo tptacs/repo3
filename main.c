9449d8222da3116420979690adfbca064077d8d1
Ro <rochy.1997@hotmail.com>
2019-09-17 01:50:32 -0300

    me tira simbolito

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int main()
 {
 	int anio = 2019;
-	char* name = "tacs";
+	char name[5];
+	strcpy(name, "tacs");
 	
 	printf("la materia es %c y el anio es %i \n", name, anio);
    
